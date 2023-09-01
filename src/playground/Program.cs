6a99375bba844283dcc5748608bc03ac8f10f503
John Campion <john@brightshore.com>
2023-09-01 02:05:32 -0400

    tweaks to program

--- src/playground/Program.cs
+++ src/playground/Program.cs
@@ -12,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.ResolveConflictingActions(a => a.Last());
+});
 
 var dbConnection = new SqliteConnection("DataSource=:memory:");
 builder.Services.AddSingleton(_ => dbConnection);
@@ -43,6 +46,7 @@ builder.Services
     ;
 
 builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<UnAuthOptions>, UnAuthConfigureOptions>());
+builder.Services.AddScoped<UnAuthTokenService>();
 
 
 
